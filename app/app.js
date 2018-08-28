f167c438aa2c195b2c0a41fc65b2e1c47323cf99
Alexander Mire <amire51117@gmail.com>
2018-08-28 18:43:04 -0400

    add logic that reacts to button click

--- app/app.js
+++ app/app.js
@@ -1,15 +1,25 @@
-// import anaTableController from "./components/anagramTable/anaTableController";
-
 angular.module("App",['main']);
 angular.module("main", []);
 
 angular.module("main").controller("mainController", ($scope) => {
 	$scope.validWord = "";
-
-
-	$scope.checkValidity = (word) => {
-		let bool = wordChecker[word];
-		return bool === undefined ? false : true;
-	}
+	$scope.anagrams = new Set();
+	$scope.anagram = "";
+	// $scope.checkValidity = (word) => {
+	// 	let bool = wordChecker[word];
+	// 	return bool === undefined ? false : true;
+	// }
 	
+
+	$scope.findAnagrams = () => {
+		//find an anagram, and add to set.
+
+		//just for starters, see if we can add 'a'
+		
+		if ($scope.anagram === "" && $scope.validWord !== "") {
+			$scope.anagram = $scope.validWord;
+		}
+
+		$scope.anagram += 'a';
+	}
 });
