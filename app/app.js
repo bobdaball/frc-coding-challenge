437ee1e2af008cf506096861f70926ed5c8f57fe
Alexander Mire <amire51117@gmail.com>
2018-08-28 19:10:08 -0400

    have basic assignment trigger on button click

--- app/app.js
+++ app/app.js
@@ -5,6 +5,10 @@ angular.module("main").controller("mainController", ($scope) => {
 	$scope.validWord = "";
 	$scope.anagrams = new Set();
 	$scope.anagram = "";
+	//previous word is necessary to see if word has changed.
+	$scope.previousWord = "";
+
+
 	// $scope.checkValidity = (word) => {
 	// 	let bool = wordChecker[word];
 	// 	return bool === undefined ? false : true;
@@ -16,10 +20,26 @@ angular.module("main").controller("mainController", ($scope) => {
 
 		//just for starters, see if we can add 'a'
 		
-		if ($scope.anagram === "" && $scope.validWord !== "") {
-			$scope.anagram = $scope.validWord;
-		}
+		// how to make anagram
+		// shuffle string
+		$scope.anagram = $scope.shuffleString($scope.validWord);
+	}
 
-		$scope.anagram += 'a';
+	$scope.shuffleString = (stringInput) => {
+		let strArr = stringInput.split('');
+		let size = stringInput.length;
+
+		for (let i = 0; i < size; i++) {
+			let randNum = $scope.randNumGenerator(i, size);
+			let temp = strArr[i];
+			strArr[i] = strArr[randNum];
+			strArr[randNum] = temp;
+		}
+		return strArr.join('');
+	}
+
+
+	$scope.randNumGenerator = (min, max) => {
+		return Math.floor( (Math.random() * (max - min)) + min);
 	}
 });
